70f823e375ad26257f10af1de83d08cd85115d35
Alexis Janvier <contact@alexisjanvier.net>
2021-05-06 06:10:32 +0200

    Bootstrap react-admin

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import { Component } from 'react'
+import * as React from 'react'
+import { Admin, Resource, ListGuesser } from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
 
-import Logo from './assets/altafino.svg'
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 
-class App extends Component {
-  render() {
-    return (
-      <div className='flex items-center justify-center h-screen'>
-        <div className='text-black font-bold rounded-lg border shadow-lg p-10 m-20'>
-          <div>
-            Webpack 5 boilerplate with React 17, Tailwind 2, using babel, sass,
-            with a hot dev server and an optimized production build.
-            <div className={'bg-red-500 text-white'}>
-              process.env.API_URL:{process.env.API_URL} !!! Make sure to put
-              both .env.* files in gitignore.
-            </div>
-            <div className={'container mx-auto px-4'}>
-              <a href={'https://altafino.com'}>
-                <Logo className={'mt-10 px-10'} />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name='users' list={ListGuesser} />
+  </Admin>
+)
 
 export default App
